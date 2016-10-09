f6d632eef610e0fcc5b59a0185cff4610d645e79
wanglei <wangleif@zbj.com>
2016-10-09 17:50:01 +0800

    判断是否有环

--- src/test/java/TestCycleList.java
+++ src/test/java/TestCycleList.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -6,8 +7,49 @@ import org.junit.Test;
  */
 public class TestCycleList {
 
-    @Test
+    private TNode head;
+
+    @Before
     public void createCycleList() {
-        Assert.fail("直接失败！");
+        TNode a = new TNode();
+        TNode b = new TNode();
+        TNode c = new TNode();
+        TNode d = new TNode();
+
+        a.setNext(b);
+        b.setNext(c);
+        c.setNext(d);
+        d.setNext(b);
+
+        head = a;
+    }
+
+    @Test
+    public void isCycleList(){
+
+        boolean isCycleList = false;
+
+        int step = 0;
+        TNode next = head;
+
+        while (true) {
+
+            if (next == null || next.getNext() == null) {
+                break;
+            }
+
+            next = next.getNext();
+            step++;
+
+            for (int i = 0; i < step; i++) {
+                TNode temp = head;
+                if(next == temp) {
+                    isCycleList = true;
+                    break;
+                }
+            }
+        }
+
+        Assert.assertFalse("这是一个带有环的链表！", isCycleList);
     }
 }
